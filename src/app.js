403ff1d0f7d01641da166270c74ddf94a761a908
chihao <chihaoyo@gmail.com>
2017-05-16 00:05:06 +0800

    Add draw index page; remove test data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-var graphs = {};
-$(function() {
-  graphs.PublicDebt = new Graph(DataSet.PublicDebt, {
-    el: '#graph0'
-  });
-});
+var app = new Vue({
+  el: '#app',
+  methods: {
+  },
+  data: {
+    common: CommonData,
+    pages: [
+      {
+        id: 'yulamcha',
+        title: '遊覽車',
+      },
+      {
+        id: '228',
+        title: '二二八',
+      },
+      {
+        id: 'tsai-first-year',
+        title: '蔡總統的第一年',
+      }
+    ]
+  }
+})
