c0957c527cb12b6b765bf14d6ff46fa6e636f47f
chihao <chihaoyo@gmail.com>
2017-05-16 17:47:13 +0800

    NO

--- src/app.js
+++ src/app.js
@@ -6,14 +6,6 @@ var app = new Vue({
     common: CommonData,
     pages: [
       {
-        id: 'yulamcha',
-        title: '遊覽車',
-      },
-      {
-        id: '228',
-        title: '二二八',
-      },
-      {
         id: 'tsai-first-year',
         title: '蔡總統的第一年',
       }
