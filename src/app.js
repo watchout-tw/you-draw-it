cb3355044679a1d1bbc86e9bf3943c455b02311c
jasjas110 <jasjas110@gmail.com>
2017-05-19 10:45:42 +0800

    debugging

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 var app = new Vue({
   el: '#app',
   methods: {
+    markdown: marked,
   },
   data: {
     common: CommonData,
+    title: '不然你來畫畫看啊',
+    description: '不靠感覺，靠資料、靠數據的新聞，有時候，並不符合大眾的預期。你也來試試？',
     pages: [
       {
         id: 'tsai-first-year',
